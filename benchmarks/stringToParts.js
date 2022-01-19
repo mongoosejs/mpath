c0f123041c4386e3ef7e951ea88a0609c19f5ece
uzlopak <aras.abbasi@googlemail.com>
2022-01-19 10:37:02 +0100

    apply linter

--- benchmarks/stringToParts.js
+++ benchmarks/stringToParts.js
@@ -3,48 +3,48 @@
 const stringToParts = require('../lib/stringToParts.js');
 const Bench = require('benchmark');
 
-const s = new Bench.Suite("stringToParts")
-  .add('stringToParts("first")', function () {
+const s = new Bench.Suite('stringToParts')
+  .add('stringToParts("first")', function() {
     stringToParts('first');
   })
-  .add('stringToParts("first.second")', function () {
+  .add('stringToParts("first.second")', function() {
     stringToParts('first.second');
   })
-  .add('stringToParts("first.second.third.1.name")', function () {
+  .add('stringToParts("first.second.third.1.name")', function() {
     stringToParts('first.second.third.1.name');
   })
-  .add('stringToParts("comments")', function () {
+  .add('stringToParts("comments")', function() {
     stringToParts('comments');
   })
-  .add('stringToParts("comments.1")', function () {
+  .add('stringToParts("comments.1")', function() {
     stringToParts('comments.1');
   })
-  .add('stringToParts("comments.2.name")', function () {
+  .add('stringToParts("comments.2.name")', function() {
     stringToParts('comments.2.name');
   })
-  .add('stringToParts("comments.2.comments.1.comments.0.val")', function () {
+  .add('stringToParts("comments.2.comments.1.comments.0.val")', function() {
     stringToParts('comments.2.comments.1.comments.0.val');
   })
-  .add('stringToParts("comments.name")', function () {
+  .add('stringToParts("comments.name")', function() {
     stringToParts('comments.name');
   })
-  .add('stringToParts("foo[1mystring]")', function () {
+  .add('stringToParts("foo[1mystring]")', function() {
     stringToParts('foo[1mystring]');
   })
-  .add('stringToParts("foo[1mystring].bar[1]")', function () {
+  .add('stringToParts("foo[1mystring].bar[1]")', function() {
     stringToParts('foo[1mystring].bar[1]');
   })
-  .add('stringToParts("foo[1mystring][2]")', function () {
+  .add('stringToParts("foo[1mystring][2]")', function() {
     stringToParts('foo[1mystring][2]');
   });
 
-s.on('start', function () {
+s.on('start', function() {
   console.log('starting...');
 })
-  .on('cycle', function (e) {
+  .on('cycle', function(e) {
     const s = String(e.target);
     console.log(s);
   })
-  .on('complete', function () {
+  .on('complete', function() {
   })
   .run();
