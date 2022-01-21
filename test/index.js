e034a4cc27b7ba51a0e50a496aa46f17464b0d88
uzlopak <aras.abbasi@googlemail.com>
2022-01-21 04:07:43 +0100

    add unit test, fix remark

--- test/index.js
+++ test/index.js
@@ -814,6 +814,14 @@ describe('mpath', function() {
             { yep: 0 }
           ], o.arr);
 
+          const testArray = {
+            array: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
+          };
+          mpath.set('array.11', 0, testArray);
+          assert.deepEqual( {
+            array: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 0, 12],
+          }, testArray);
+
           done();
         });
       });
